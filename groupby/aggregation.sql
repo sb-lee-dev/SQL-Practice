989178780e2f5971d664cbfb95915af74438f561
seongbaelee <slee617seneca@gmail.com>
2026-01-07 20:30:44 -0500

    aggregation functions

---
+++ groupby/aggregation.sql
@@ -0,0 +1,5 @@
+SELECT MIN(replacement_cost) FROM film
+SELECT MAX(replacement_cost) FROM film
+SELECT COUNT(replacement_cost) FROM film
+SELECT AVG(replacement_cost) FROM film
+SELECT SUM(replacement_cost) FROM film
