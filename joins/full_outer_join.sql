da5fde333529560f271f1a12896993dbd4a1436f
seongbaelee <slee617seneca@gmail.com>
2026-01-08 21:09:20 -0500

    fix typo on full_outer_join.sql

--- joins/full_outer_join.sql
+++ joins/full_outer_join.sql
@@ -1,4 +1,4 @@
 SELECT * FROM customer
 FULL OUTER JOIN payment
 ON customer.customer_id = payment.customer_id
-WHERE customer.customer_id IS null OR payment.customer_id IS null
+WHERE customer.customer_id IS null OR payment.payment_id IS null
